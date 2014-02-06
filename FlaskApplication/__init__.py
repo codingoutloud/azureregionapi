efda2a45448f40e3c3b9800f7cc945d32fec81e2
Bill Wilder <codingoutloud@gmail.com>
2014-02-05 19:24:12 -0500

    try to unbreak (prob dependencies?)

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
-from flask import jsonify, request
-from azureip import AzureIp
+#from flask import jsonify, request
+#from azureip import AzureIp
 
 app = Flask(__name__)
-azure_ip = AzureIp()
+#azure_ip = AzureIp()
 
 
 @app.route('/')
@@ -21,15 +21,15 @@ def home():
 
 @app.route('/regions/<ip_address>', methods=["GET"])
 def get_region(ip_address):
-    region = azure_ip.find_ip(ip_address)
-    if region is None:
-        return jsonify(region='<unknown>'), 404
-    else:
-        return jsonify(region=region)
+    #region = azure_ip.find_ip(ip_address)
+    #if region is None:
+        #return jsonify(region='<unknown>'), 404
+    #else:
+        #return jsonify(region=region)
 
 
 app.debug = True
-app.debug = False
+#app.debug = False
 if __name__ == "__main__":
     app.run()
 
